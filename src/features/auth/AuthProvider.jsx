16338b724d42608cefb66631685ed2d3741f7d38
utkarsh etar <etarutkarsh@gmail.com>
2025-12-11 14:35:25 +0000

    updates

--- src/features/auth/AuthProvider.jsx
+++ src/features/auth/AuthProvider.jsx
@@ -18,13 +18,13 @@ import { mergeGuestDataOnLogin } from "../cart/cartSlice";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAs6jl1ahYeBu1XTyZYzcsfBYBeuCsaUbE",
-  authDomain: "e-commerceapplication-f3c61.firebaseapp.com",
-  projectId: "e-commerceapplication-f3c61",
-  storageBucket: "e-commerceapplication-f3c61.firebasestorage.app",
-  messagingSenderId: "22624828098",
-  appId: "1:22624828098:web:af61c65cc01ac9dca5d95b",
-  measurementId: "G-KGEDESZZLY",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: true, // allow external/dev URLs
+    strictPort: true,
+    allowedHosts: true, // allow ALL Codesandbox hosts
   },
 });
