363dc08339890c33128b80751b74877a47c61ebe
utkarsh etar <etarutkarsh@gmail.com>
2025-12-11 13:40:34 +0000

    changes in the vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
   server: {
-    allowedHosts: [
-      "localhost",
-      "127.0.0.1",
-      "83t5gx-5173.csb.app", // ← ADD THIS
-    ],
+    host: true,
   },
 });
